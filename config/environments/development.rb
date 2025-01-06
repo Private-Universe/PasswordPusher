7621e0e8d70dc4104d40dc52862d4c9a1df1b50a
Lucas Pathuis <Lucas.Pathuis@privateuniverse.com.au>
2025-01-06 23:44:50 +1100

    update codebase, containers and packages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,10 +1,14 @@
+# frozen_string_literal: true
+
+require "active_support/core_ext/integer/time"
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # In the development environment your application's code is reloaded on
-  # every request. This slows down response time but is perfect for development
+  # In the development environment your application's code is reloaded any time
+  # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.enable_reloading = true
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -12,15 +16,18 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Enable server timing
+  config.server_timing = true
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+  if Rails.root.join("tmp/caching-dev.txt").exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -29,39 +36,60 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = Settings.files.storage
 
-  config.action_mailer.raise_delivery_errors = Settings.mail.raise_delivery_errors
+  if Settings.mail
+    config.action_mailer.perform_caching = false
 
-  config.action_mailer.perform_caching = false
+    config.action_mailer.raise_delivery_errors = Settings.mail.raise_delivery_errors
 
-  config.action_mailer.default_url_options = { host: '127.0.0.1:5100', protocol: 'https' }
+    config.action_mailer.default_url_options = {host: "127.0.0.1:5100", protocol: "https"}
 
-  config.action_mailer.smtp_settings = {
-    address: Settings.mail.smtp_address,
-    port: Settings.mail.smtp_port,
-    user_name: Settings.mail.smtp_user_name,
-    password: Settings.mail.smtp_password,
-    authentication: Settings.mail.smtp_authentication,
-    enable_starttls_auto: Settings.mail.smtp_enable_starttls_auto,
-    open_timeout: Settings.mail.smtp_open_timeout,
-    read_timeout: Settings.mail.smtp_read_timeout
-  }
+    config.action_mailer.smtp_settings = {
+      address: Settings.mail.smtp_address,
+      port: Settings.mail.smtp_port
+    }
 
-  if Settings.mail.smtp_domain
-    config.action_mailer.smtp_settings[:domain] = Settings.mail.smtp_domain
+    config.action_mailer.smtp_settings[:domain] = Settings.mail.smtp_domain if Settings.mail.smtp_domain
+    config.action_mailer.smtp_settings[:open_timeout] = Settings.mail.smtp_open_timeout if Settings.mail.smtp_open_timeout
+    config.action_mailer.smtp_settings[:read_timeout] = Settings.mail.smtp_read_timeout if Settings.mail.smtp_read_timeout
+
+    if !Settings.mail.smtp_authentication.nil?
+      config.action_mailer.smtp_settings[:authentication] = Settings.mail.smtp_authentication
+    end
+
+    if !Settings.mail.smtp_user_name.nil?
+      config.action_mailer.smtp_settings[:user_name] = Settings.mail.smtp_user_name
+    end
+
+    if !Settings.mail.smtp_password.nil?
+      config.action_mailer.smtp_settings[:password] = Settings.mail.smtp_password
+    end
+
+    if !Settings.mail.smtp_openssl_verify_mode.nil?
+      config.action_mailer.smtp_settings[:openssl_verify_mode] = Settings.mail.smtp_openssl_verify_mode.to_sym
+    end
+
+    if !Settings.mail.smtp_enable_starttls_auto.nil?
+      config.action_mailer.smtp_settings[:enable_starttls_auto] = Settings.mail.smtp_enable_starttls_auto
+    end
+
+    if !Settings.mail.smtp_enable_starttls.nil?
+      config.action_mailer.smtp_settings[:enable_starttls] = Settings.mail.smtp_enable_starttls
+    end
   end
 
-  if Settings.mail.smtp_openssl_verify_mode
-    config.action_mailer.smtp_settings[:openssl_verify_mode] = Settings.mail.smtp_openssl_verify_mode.to_sym
-  end
-
-  if Settings.mail.smtp_enable_starttls
-    config.action_mailer.smtp_settings[:enable_starttls] = Settings.mail.smtp_enable_starttls
-  end
+  config.logger = Logger.new($stdout) if Settings.log_to_stdout
+  config.log_level = :debug
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
+
+  # Raise exceptions for disallowed deprecations.
+  config.active_support.disallowed_deprecation = :raise
+
+  # Tell Active Support which deprecation messages to disallow.
+  config.active_support.disallowed_deprecation_warnings = []
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
@@ -69,18 +97,48 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
-  config.assets.debug = true
+  # Highlight code that enqueued background job in logs.
+  config.active_job.verbose_enqueue_logs = true
+
+  # Use a real queuing backend for Active Job (and separate queues per environment).
+  config.active_job.queue_adapter = :solid_queue
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
   # Raises error for missing translations.
-  # config.action_view.raise_on_missing_translations = true
+  config.i18n.raise_on_missing_translations = true
 
-  # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # Annotate rendered view with file names.
+  # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Uncomment if you wish to allow Action Cable access from any origin.
+  # config.action_cable.disable_request_forgery_protection = true
+
+  # Raise error when a before_action's only/except options reference missing actions
+  config.action_controller.raise_on_missing_callback_actions = true
+
+  # The list of trusted proxies from which we will accept proxy related headers.
+  config.action_dispatch.trusted_proxies = [
+    "127.0.0.1",         # Localhost
+    /^::1$/,             # IPv6 localhost
+    /192\.168\.\d{1,3}\.\d{1,3}/, # Local network
+    /10\.\d{1,3}\.\d{1,3}\.\d{1,3}/ # Private networks
+  ]
+
+  if Settings.trusted_proxies.present?
+    trusted_proxies = Settings.trusted_proxies.split(",").map(&:strip)
+    config.action_dispatch.trusted_proxies.concat(trusted_proxies)
+  end
+
+  # If a user sets the allowed_hosts setting, we need to add the domain(s) to the list of allowed hosts
+  if Settings.allowed_hosts.present?
+    if Settings.allowed_hosts.is_a?(Array)
+      config.hosts.concat(Settings.allowed_hosts)
+    elsif Settings.allowed_hosts.is_a?(String)
+      config.hosts.concat Settings.allowed_hosts.split
+    else
+      raise "Settings.allowed_hosts (PWP__ALLOWED_HOSTS): Allowed hosts must be an array or string"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,24 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  apipie
-  localized do
-    devise_for :users, skip: :registrations, controllers: {
-      sessions: 'users/sessions',
-      passwords: 'users/passwords',
-      unlocks: 'users/unlocks',
-      confirmations: 'users/confirmations'
-    }
+  if ENV.key?("PWP_PUBLIC_GATEWAY")
+    draw :public_users
+    draw :public_pushes
 
-    devise_scope :user do
-      resource  :registration,
-                only: %i[new create edit update],
-                path: 'users',
-                path_names: { new: 'sign_up' },
-                controller: 'users/registrations',
-                as: :user_registration do
-                  get :cancel
-                  get :token
-                  delete :token, action: :regen_token
-                end
-    end
+    # Add a route that handles the root path and returns a 404 error
+    root to: proc { |env| [404, {"Content-Type" => "text/html"}, ["<h1>Not Found</h1>"]] }
+  else
+    draw :admin
+    draw :users
+    draw :pushes
+    apipie
 
-    get '/d/active' => 'dashboard#active', as: :dashboard_active
-    get '/d/expired' => 'dashboard#expired', as: :dashboard_expired
+    get "/pages/*id" => "pages#show", :as => :page, :format => false
 
-    resources :p, controller: :passwords, as: :passwords, except: %i[index edit update] do
-      get 'preview', on: :member
-      get 'r', on: :member, as: 'preliminary', action: 'preliminary'
-      get 'audit', on: :member
-    end
+    draw :legacy_devise
+    draw :legacy_pages
+    draw :legacy_pushes
 
-    resources :c, controller: :commands, as: :commands, allow: %i[create]
-    get '/slack_direct_install', to: redirect("https://slack.com/oauth/authorize?client_id=#{SLACK_CLIENT_ID}&scope=commands", status: 302)
-    get '/pages/*id' => 'pages#show', as: :page, format: false
-    resources :feedbacks, only: %i[new create]
-    root to: 'passwords#new', :locale => I18n.default_locale
+    root to: "passwords#new"
   end
 end
